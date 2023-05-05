46f6cc5244189411866b83ea874f78f4be9a5306
GUILHERME HENRIQUE SANCHES ANJOLETTE <guilherme.anjolette>
2023-05-04 21:21:45 -0300

    aula 04-05-23

---
+++ components/apostila_03/index.js
@@ -0,0 +1,11 @@
+import {View, Text } from 'react-native';
+
+import styles from './style';
+
+export default function apostila03() {
+    return(
+        <View style={styles.container} >
+            <Text>  Exemplo 3  </Text>
+        </View>
+    );
+};
